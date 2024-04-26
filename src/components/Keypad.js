b7265e632c1d67744264741dc24fb14b95cfd85d
James <jamonjoroge816@gmail.com>
2024-04-26 14:31:54 +0300

    EventHandling-lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,20 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+    function handleChange(){
+        console.log('Entering password...')
+    };
     return (
-        <div></div>
+        <div>
+            <input
+                type="password"
+                onChange={handleChange}
+                placeholder="Enter password"
+
+
+            />
+
+        </div>
     )
 }
 
